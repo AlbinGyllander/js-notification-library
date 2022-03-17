eda2f1e5a8004c283a0c127a2662b50b93deb076
AlbinGyllander <77164660+AlbinGyllander@users.noreply.github.com>
2022-03-17 21:50:59 +0100

    test

---
+++ notification.js
@@ -0,0 +1,112 @@
+const DEFAULT_OPTIONS = {
+    position: "top-right",
+    text:'default text',
+    timeToShow: 5000,
+    autoClose: 3000,
+    close:true,
+    priorityType: 'standard',
+}
+const ALERT_COLORS = {
+    standard:'blue',
+    alert:'red',
+    warning:'red',
+}
+export default class notification{
+    #notificationElement
+    #animFrame
+    #text
+    #textElement
+    #removeBinded 
+    #priorityType
+    
+    constructor(options){
+        this.#notificationElement = document.createElement("div")
+        this.#notificationElement.classList.add('notification')
+        
+        requestAnimationFrame(() =>  {
+            this.#notificationElement.classList.add('show') 
+        })
+        this.#textElement = document.createElement('p')
+        this.#notificationElement.append(this.#textElement)
+        
+        this.#removeBinded = this.delete.bind(this)
+        this.update({...DEFAULT_OPTIONS,...options})
+        
+    }
+    
+    set position(value){
+        const currentContainer = this.#notificationElement.parentElement
+        const selector = `.notification-container[data-position="${value}"]`
+        const container = document.querySelector(selector) || createContainer(value)
+        container.append(this.#notificationElement)
+        if (currentContainer == null || currentContainer.hasChildNodes()) return
+        currentContainer.remove()
+    }
+
+    set text(value){
+        this.#text = value
+        this.#textElement.innerHTML = this.#text
+    }
+
+    set close(value){
+        if(value){
+            this.#notificationElement.addEventListener("click", this.#removeBinded)
+        }else{
+            this.#notificationElement.removeEventListener("click",this.#removeBinded)
+        }
+    }
+
+    set priorityType(value){
+        this.#priorityType = value
+        this.#notificationElement.classList.add(`notification-priority-${this.#priorityType}`)
+        var r = document.querySelector(':root');
+        r.style.setProperty('--priorityLevel', ALERT_COLORS[this.#priorityType]); 
+    }
+
+    set autoClose(value){
+        if(value === false)return
+        let startTime = 0
+        const repeat = time =>{
+            startTime += 15
+            if(startTime >= value){
+                cancelAnimationFrame(this.#animFrame)
+                this.delete()
+                return
+            }
+            this.#animFrame = requestAnimationFrame(repeat);
+        }
+        this.#animFrame = requestAnimationFrame(repeat)
+    }
+    
+
+    delete(){
+        const container = this.#notificationElement.parentElement
+        this.#notificationElement.classList.remove('show')
+        this.#notificationElement.addEventListener('transitionend', () => {
+            this.#notificationElement.remove()
+        })
+        
+        if(container!= null && container.hasChildNodes() == false) container.remove()  
+    }
+
+    update(options) {
+        Object.entries(options).forEach(([key, value]) => {
+          this[key] = value
+        })
+        return options
+    }
+    
+    
+}
+
+
+function createContainer(position) {
+    const container = document.createElement("div")
+    container.classList.add("notification-container")
+    container.dataset.position = position
+    document.body.append(container)
+    return container
+}
+
+
+
--- script.js
+++ script.js
@@ -1,112 +1,16 @@
-const DEFAULT_OPTIONS = {
-    position: "top-right",
-    text:'default text',
-    timeToShow: 5000,
-    autoClose: 3000,
-    close:true,
-    priorityType: 'standard',
-}
-const ALERT_COLORS = {
-    standard:'blue',
-    alert:'red',
-    warning:'red',
-}
-export default class notification{
-    #notificationElement
-    #animFrame
-    #text
-    #textElement
-    #removeBinded 
-    #priorityType
+
+import notification from "https://albingyllander.github.io/js-notification-library/notification.js"
+document.querySelector("button").addEventListener("click", () => {
     
-    constructor(options){
-        this.#notificationElement = document.createElement("div")
-        this.#notificationElement.classList.add('notification')
+    const available_position = ['top-right','top-left','top-center','bottom-right','bottom-left','bottom-center']
+    const available_priority= ['alert','standard','warning']
+    const notificationItem = new notification({
         
-        requestAnimationFrame(() =>  {
-            this.#notificationElement.classList.add('show') 
-        })
-        this.#textElement = document.createElement('p')
-        this.#notificationElement.append(this.#textElement)
-        
-        this.#removeBinded = this.delete.bind(this)
-        this.update({...DEFAULT_OPTIONS,...options})
-        
-    }
-    
-    set position(value){
-        const currentContainer = this.#notificationElement.parentElement
-        const selector = `.notification-container[data-position="${value}"]`
-        const container = document.querySelector(selector) || createContainer(value)
-        container.append(this.#notificationElement)
-        if (currentContainer == null || currentContainer.hasChildNodes()) return
-        currentContainer.remove()
-    }
+        position: Math.floor(Math.random() * (6  - 1 + 1) + 1),
+        autoClose:3000,
+        text:'Incorrect login',
+        close:true,
+        priorityType: Math.floor(Math.random() * (3  - 1 + 1) + 1),
 
-    set text(value){
-        this.#text = value
-        this.#textElement.innerHTML = this.#text
-    }
-
-    set close(value){
-        if(value){
-            this.#notificationElement.addEventListener("click", this.#removeBinded)
-        }else{
-            this.#notificationElement.removeEventListener("click",this.#removeBinded)
-        }
-    }
-
-    set priorityType(value){
-        this.#priorityType = value
-        this.#notificationElement.classList.add(`notification-priority-${this.#priorityType}`)
-        var r = document.querySelector(':root');
-        r.style.setProperty('--priorityLevel', ALERT_COLORS[this.#priorityType]); 
-    }
-
-    set autoClose(value){
-        if(value === false)return
-        let startTime = 0
-        const repeat = time =>{
-            startTime += 15
-            if(startTime >= value){
-                cancelAnimationFrame(this.#animFrame)
-                this.delete()
-                return
-            }
-            this.#animFrame = requestAnimationFrame(repeat);
-        }
-        this.#animFrame = requestAnimationFrame(repeat)
-    }
-    
-
-    delete(){
-        const container = this.#notificationElement.parentElement
-        this.#notificationElement.classList.remove('show')
-        this.#notificationElement.addEventListener('transitionend', () => {
-            this.#notificationElement.remove()
-        })
-        
-        if(container!= null && container.hasChildNodes() == false) container.remove()  
-    }
-
-    update(options) {
-        Object.entries(options).forEach(([key, value]) => {
-          this[key] = value
-        })
-        return options
-    }
-    
-    
-}
-
-
-function createContainer(position) {
-    const container = document.createElement("div")
-    container.classList.add("notification-container")
-    container.dataset.position = position
-    document.body.append(container)
-    return container
-}
-
-
-
+    })
+})
